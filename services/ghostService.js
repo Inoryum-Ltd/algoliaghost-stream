5076051aee7c75c032146af32f7331fff6995049
Salehin Khan <saleheen04@gmail.com>
2024-05-06 20:11:04 +0600

    ghost content api updated

--- services/ghostService.js
+++ services/ghostService.js
@@ -4,7 +4,7 @@ const { ghostApiUrl, ghostContentApiKey } = require('../config/config');
 const api = new GhostContentAPI({
   url: ghostApiUrl,
   key: ghostContentApiKey,
-  version: 'v5'
+  version: "v5.0"
 });
 
 module.exports = { api };
